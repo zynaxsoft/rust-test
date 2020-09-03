b18145d39542f2f1debe3aa37a7da6562356371a
Tanapol Prucksakorn <tanapol.pr@gmail.com>
2020-09-04 07:44:10 +0900

    playing with trait objects

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,81 @@
-use std::convert::TryInto;
-use test2::ABC;
+// #![allow(unused_imports)]
+// use color_eyre::{eyre::Report, eyre::WrapErr, Result, Section};
+// use log::{debug, info, trace, warn};
+// use thiserror::Error;
+// 
+// mod util;
+// fn main() -> Result<()> {
+//     color_eyre::install()?;
+//     util::setup_logger()?;
+// 
+//     Ok(())
+// }
 
 fn main() {
-    let mut a = std::num::Wrapping(20i8);
-    a += std::num::Wrapping(127);
-    println!("{}", a);
+    let kos = Kos(10);
+    let zin = Zin("woah".into());
+
+    let kos_dyn = dispatch_shake(&kos);
+    let zin_dyn = dispatch_shake(&zin);
+
+    downcast_shake(kos_dyn);
+    downcast_shake(zin_dyn);
+}
+
+struct Kos(u8);
+struct Zin(String);
+
+trait Shakable {
+    fn shake(&self);
+}
+
+impl Shakable for Kos {
+    fn shake(&self) {
+        println!("Kos shake {}.", self.0);
+    }
+}
+
+impl Shakable for Zin {
+    fn shake(&self) {
+        println!("Zin shake {}.", self.0);
+    }
+}
+
+fn dispatch_shake<T>(s: &T) -> &dyn Ultima where T: Ultima {
+    s.shake();
+    s
+}
+
+enum ShakeType {
+    Kos,
+    Zin,
+}
+
+trait Downcast {
+    fn downcast(&self) -> ShakeType;
+}
+
+trait Ultima: Shakable + Downcast {
+}
+
+impl Ultima for Kos {}
+impl Ultima for Zin {}
+
+impl Downcast for Kos {
+    fn downcast(&self) -> ShakeType {
+        ShakeType::Kos
+    }
+}
+
+impl Downcast for Zin {
+    fn downcast(&self) -> ShakeType {
+        ShakeType::Zin
+    }
+}
+
+fn downcast_shake(s: &dyn Ultima) {
+    match s.downcast() {
+        ShakeType::Kos => println!("Got Kos"),
+        ShakeType::Zin => println!("Got Zin"),
+    }
 }
---
+++ src/util.rs
@@ -0,0 +1,21 @@
+use fern::colors::{Color, ColoredLevelConfig};
+
+pub fn setup_logger() -> Result<(), fern::InitError> {
+    let mut colors = ColoredLevelConfig::new()
+        .info(Color::Green);
+    colors.warn = Color::Magenta;
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                colors.color(record.level()),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply()?;
+    Ok(())
+}
